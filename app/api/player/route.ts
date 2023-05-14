5d4791a3d8ad1951eed54c0e48cd3f3f64ffe2d8
georgwindhaber <georg.windhaber@gmail.com>
2023-05-14 17:04:09 +0200

    Add placebo logic to player route

--- app/api/player/route.ts
+++ app/api/player/route.ts
@@ -4,5 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const players = await prisma.player.findMany();
 
+  players.sort((a, b) => b.score - a.score);
+
   return NextResponse.json({ response: "ok", data: players });
 }
