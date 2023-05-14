52f221c6379b01790c84b84fa66ec73e8ad7989e
georgwindhaber <georg.windhaber@gmail.com>
2023-05-14 17:21:25 +0200

    Make player api dynamic

--- app/api/player/route.ts
+++ app/api/player/route.ts
@@ -1,10 +1,26 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const players = await prisma.player.findMany();
+type PlayerRequest = {
+  body?: {
+    ids?: number[];
+  };
+} & Request;
 
-  players.sort((a, b) => b.score - a.score);
+export async function GET(request: PlayerRequest) {
+  let players = [];
+  if (request?.body?.ids) {
+    const ids = request.body.ids;
+    players = await prisma.player.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+  } else {
+    players = await prisma.player.findMany();
+  }
 
-  return NextResponse.json({ response: "ok", data: players });
+  return NextResponse.json({ response: "ok", data: players, request });
 }
